2303714873285d69f410dafe173b1d3a84b56df6
Diman <84639576+SuperErnD@users.noreply.github.com>
2022-09-03 20:52:49 +0300

    Delete MainSite directory

--- MainSite/www/_404.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Diman\Openlive\Pages;
-
-require './sys/prepare.php';
-
-http_response_code(404);
-trigger_error('404', E_USER_NOTICE);
-exit("ты дебил, страница не найдена");
--- MainSite/www/assets/js/login.js
+++
@@ -1,8 +0,0 @@
-function SendRequest(url, method, headers, body) {
-  return new Promise((a, b) => {
-    fetch(url, { method, headers: { "User-Agent": `Official site, ${url}`, ...headers }, body: body ? body : undefined })
-        .then(a)
-        .catch(b);
-  });
-}
-
