34b449eab38c19ed720728670c709f3770b2f3a0
Diman <84639576+SuperErnD@users.noreply.github.com>
2022-09-03 20:53:13 +0300

    Delete sys/conf directory

--- sys/conf/DatabaseConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-    Open Live Database Config 
-*/
-
-// Main database config
-$MainDatabase = 'sqlite:'.__DIR__.'/../database.sqlite'; // mysql:host=localhost;dbname=test for example
--- sys/conf/MessengerServerConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-// DO NOT EDIT
-$SupportedVersions = [1];
-$ErrorCodes = [
-    "VersionParseError" => "0x0000f14"
-];
