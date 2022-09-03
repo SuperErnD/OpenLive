65112067e0cd7f6a3a3f3726e9e94bb38ee3b78f
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-04 01:53:35 +0700

    php7 fix

--- Web/autoload.php
+++ Web/autoload.php
@@ -1,9 +1,12 @@
 <?php
+$phpversion = (float)phpversion();
 
 // CORE
 require __DIR__ . "/core/router.php";
 require __DIR__ . "/core/db.php";
-
+if ($phpversion < 8.0 && $phpversion >= 6.0){
+	require __DIR__ . "/core/php7.php";
+}
 
 // CONTROLLERS
 require __DIR__ . "/controllers/BaseController.php";
---
+++ Web/core/php7.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Diman\Openlive\core;
+
+function str_starts_with( $haystack, $needle ) {
+     $length = strlen( $needle );
+     return substr( $haystack, 0, $length ) === $needle;
+}
