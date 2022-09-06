13ba1c6a6ce0dd8cc2d62ef981b360fa22dafc1e
Diman <84639576+SuperErnD@users.noreply.github.com>
2022-09-06 09:35:16 +0300

    Update MainController.php

--- Web/controllers/MainController.php
+++ Web/controllers/MainController.php
@@ -6,4 +6,12 @@ class MainController extends BaseController{
     public function indexAction() {
         echo $this->render("index", []);
     }
+    
+    public function ToSAction() {
+        echo $this->render("legaly/tos", []);
+    }
+    
+    public function PrivacyAction() {
+        echo $this->render("legaly/privacy", []);
+    }
 }
