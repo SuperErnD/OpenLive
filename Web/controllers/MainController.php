ae0c19fd9db94219b7543915c6e64338e0ad0717
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-07 09:40:36 +0700

    remove tos and privacy action from main controller

--- Web/controllers/MainController.php
+++ Web/controllers/MainController.php
@@ -6,12 +6,4 @@ class MainController extends BaseController{
     public function indexAction() {
         echo $this->render("index", []);
     }
-    
-    public function tosAction() {
-        echo $this->render("legaly/tos", []);
-    }
-    
-    public function privacyAction() {
-        echo $this->render("legaly/privacy", []);
-    }
 }
