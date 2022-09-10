b978b977f7f872b4ecb190781ce12b65d75976a3
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-10 12:54:15 +0700

    add REDIRECT_URL if not exists

--- Web/controllers/AdminController.php
+++ Web/controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace Diman\Openlive\controllers;
 class AdminController extends BaseController{
 
     public function indexAction() {
+        $_SERVER['REDIRECT_URL']=$_SEVRER['REDIRECT_URL'] ?? $_SERVER['PATH_INFO'];
         echo $this->render("admin/index", [
             "users" => $this->me->get_all(),
             "delete_user" => [$this->model, "delete_user"],
