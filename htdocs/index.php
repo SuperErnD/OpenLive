f4c8f5b8b77f6c00dd4a454756b159593296f6f7
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-10 15:24:56 +0700

    .

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,7 +9,7 @@ require __DIR__ . "/../Web/conf/routes.php";
 use Tracy\Debugger;
 use Diman\Openlive\core\Router;
 
-session_save_path(__DIR__ . "/storage/sessions");
+session_save_path(__DIR__ . "/../Web/storage/sessions");
 session_start();
 
 Debugger::enable(Debugger::DEVELOPMENT);
