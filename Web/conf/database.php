6b86370f7f9fc7c4fa0c61a560e2c21d81589d68
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-10 10:48:30 +0700

    database type detect

--- Web/conf/database.php
+++ Web/conf/database.php
@@ -1,5 +1,13 @@
 <?php
 $DatabaseURL = getenv("DATABASE");
 if(!$DatabaseURL) {
-  $DatabaseURL = "sqlite:" . __DIR__ . "/../storage/database.db";
+	$DatabaseURL = "sqlite:" . __DIR__ . "/../storage/database.db";
+}
+$db_type_regex = '/mysql|sqlite/m';
+preg_match_all($db_type_regex, $DatabaseURL, $matches, PREG_SET_ORDER, 0);
+$db_type=$matches[0] ?? null;
+if ($db_type){
+	$DatabaseType=$matches[0]
+} else{
+	throw new \LogicException("unknown type of database or this database not support!");
 }
