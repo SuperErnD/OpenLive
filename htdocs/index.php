ec39ce6a718c82f1b4437a54e00cd955dbc2fc6d
Diman <84639576+SuperErnD@users.noreply.github.com>
2022-09-10 10:30:28 +0300

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,18 @@
+<?php 
+require __DIR__ . "/../vendor/autoload.php";
 
+require __DIR__ . "/../Web/autoload.php";
+
+require __DIR__ . "/../Web/conf/routes.php";
+
+
+use Tracy\Debugger;
+use Diman\Openlive\core\Router;
+
+session_save_path(__DIR__ . "/storage/sessions");
+session_start();
+
+Debugger::enable(Debugger::DEVELOPMENT);
+$router = new Router($routes);
+$router->run();
+?>
